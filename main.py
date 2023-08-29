e87c7fcfc5a558af67843c24852e7495ae527a86
Joshua Weidanz <joshua@weidanz.de>
2023-08-29 16:27:56 +0200

    improved visuals and fixed banwaving issues

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import requests
 import asyncio
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
 load_dotenv()
@@ -14,6 +15,20 @@ STAFF_CHANNEL = int(os.getenv("STAFF_CHANNEL"))
 last_staff_total = 0
 last_watchdog_total = 0
 
+async def send_increase_messages(channel, title, total, increase, description):
+    current_unix_timestamp = int(datetime.now().timestamp())
+    for _ in range(increase):
+        embed = discord.Embed(
+            title=f"{title}",
+            description=f"Ban Time: <t:{current_unix_timestamp}:R>\n{description}",
+            color=0xff0000 if "Staff" in title else 0x5b4295
+        )
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1004630104019837008/1146043716558131310/image0.webp" if "Staff" in title else "https://cdn.discordapp.com/attachments/1004630104019837008/1146043388559364206/image0.jpg")
+        embed.set_footer(text="schummelware.com", icon_url="https://cdn.discordapp.com/icons/1133857876209836063/201545faaa922146e6489583ae735b70.webp?size=160")
+        await channel.send(embed=embed)
+        print(f"{title} #{total}")  # Print the increase to the console
+        total -= 1  # Decrement total for the next message
+
 async def check_punishment_stats():
     global last_staff_total, last_watchdog_total
 
@@ -25,28 +40,18 @@ async def check_punishment_stats():
             if data['success']:
                 staff_total = data['staff_total']
                 watchdog_total = data['watchdog_total']
+                staff_increase = staff_total - last_staff_total
+                watchdog_increase = watchdog_total - last_watchdog_total
 
                 if last_staff_total != 0 and staff_total > last_staff_total:
                     channel = client.get_channel(STAFF_CHANNEL)
                     if channel:
-                        staff_banned = discord.Embed(
-                            title=f"Staff ban #{staff_total}",
-                            description="This was a staff ban. Be careful!",
-                            color=0xff0000
-                        )
-                        await channel.send(embed=staff_banned)
-                        print(f"Staff ban #{staff_total}. This was a staff ban. Be careful!")
+                        await send_increase_messages(channel, "Staff ban", staff_total, staff_increase, "They on your ass bruh")
 
                 if last_watchdog_total != 0 and watchdog_total > last_watchdog_total:
                     channel = client.get_channel(ANTICHEAT_CHANNEL)
                     if channel:
-                        watchdog_banned = discord.Embed(
-                            title=f"Anticheat ban #{watchdog_total}",
-                            description="Don't worry, this was an anticheat ban.",
-                            color=0xff0000
-                        )
-                        await channel.send(embed=watchdog_banned)
-                        print(f"Watchdog ban #{watchdog_total}. Don't worry, this was an anticheat ban.")
+                        await send_increase_messages(channel, "Watchdog ban", watchdog_total, watchdog_increase, "Nice config idiot")
 
                 last_staff_total = staff_total
                 last_watchdog_total = watchdog_total
@@ -54,7 +59,7 @@ async def check_punishment_stats():
         except Exception as e:
             print(f'Error occurred while checking punishment stats: {e}')
 
-        await asyncio.sleep(1)  # Check every second
+        await asyncio.sleep(1)
 
 client = discord.Client()
 
